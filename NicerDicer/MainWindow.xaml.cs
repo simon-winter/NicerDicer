4c21745bd1df7040ce772c8d056e593f334188ec
simon-winter <34577718+simon-winter@users.noreply.github.com>
2020-07-15 16:26:24 +0200

    scaling, exploding dice

--- NicerDicer/MainWindow.xaml.cs
+++ NicerDicer/MainWindow.xaml.cs
@@ -1,59 +1,171 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
 using System.Windows.Media;
 using NicerDicer.Properties;
 
 namespace NicerDicer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow() {            
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {        
+        private Thickness minBorderThick = new Thickness(0.5f);
+        private Thickness maxBorderThick = new Thickness(2);
+
+        int diceAmount = 6;
+        int diceValueCap = 14;
+
+        private string _prefix;
+        public string Prefix {
+            get
+            {
+                return _prefix;
+            }
+            set
+            {
+                _prefix = value;
+                RaisePropertyChanged("Prefix");
+                ExampleOutput = BuildDiceString(1, 6);
+                Settings.Default.Prefix = value;
+                Settings.Default.Save();
+            }
+        }
+
+
+        private bool _explodingDice = false;
+        public bool ExplodingDice {
+            get
+            {
+                return _explodingDice;
+            }
+            set
+            {
+                _explodingDice = value;
+                RaisePropertyChanged("ExplodingDice");
+                ExampleOutput = BuildDiceString(1, 6);
+                Settings.Default.ExplodingDice = value;
+                Settings.Default.Save();
+            }
+        }
+
+        private string _exampleOutput;
+
+
+        public string ExampleOutput {
+            get
+            {
+                return _exampleOutput;
+            }
+            set
+            {
+                _exampleOutput = value;
+                RaisePropertyChanged("ExampleOutput");
+            }
+        }
+
+        public MainWindow() {
+
             InitializeComponent();
 
-            CommandTextBox.Text = Settings1.Default.Command;
-            ExampleLabel.Text = $"{CommandTextBox.Text}1d6";
-            CommandTextBox.TextChanged += OnCommandTextChanged;            
+            this.SizeChanged += OnWindowSizeChanged;
 
-            for (int amount = 1; amount <= diceGrid.ColumnDefinitions.Count; amount++) {
+            Prefix = Settings.Default.Prefix;
+            ExplodingDice = Settings.Default.ExplodingDice;
+
+            BuildDiceGrid();
+        }
+
+        private void BuildDiceGrid() {
+
+            // add Gridcells
+            for (int amount = 1; amount <= diceAmount; amount++) {
+                diceGrid.ColumnDefinitions.Add(new ColumnDefinition() {
+                    Width = new GridLength(1, GridUnitType.Star)
+                });
+            }
+            for (int row = 1; row <= (diceValueCap / 2); row++) {
+                diceGrid.RowDefinitions.Add(new RowDefinition() {
+                    Height = new GridLength(1, GridUnitType.Star)
+                });
+            }
+
+            // populate Gridcells
+            for (int diceAmount = 1; diceAmount <= diceGrid.ColumnDefinitions.Count; diceAmount++) {
                 for (int row = 1; row <= diceGrid.RowDefinitions.Count; row++) {
-                    int value = row * 2;
-                    Button bt = new Button() {
-                        Content = $"{amount}d{value}"
+                    int diceSize = row * 2;
+                    Button btn = new Button() {
+                        Content = $"{diceAmount}d{diceSize}",
+                        BorderThickness = new Thickness(2),
+                        MinWidth = 10,
+                        Margin = new Thickness(5)
                     };
-                    Grid.SetRow(bt, row - 1);
-                    Grid.SetColumn(bt, amount - 1);
-                    bt.Margin = new Thickness(2, 10, 2, 10);
-                    bt.Click += OnButtonClicked;
-                    diceGrid.Children.Add(bt);
+                    Grid.SetRow(btn, row - 1);
+                    Grid.SetColumn(btn, diceAmount - 1);
+                    btn.Click += OnButtonClicked;
+                    diceGrid.Children.Add(btn);
                 }
             }
         }
 
-        private void OnCommandTextChanged(object sender, EventArgs e) {
-            if (sender is TextBox txt) {
-                ExampleLabel.Text = $"{txt.Text}1d6";
-                Settings1.Default.Command = txt.Text;
-                Settings1.Default.Save();
+        private string BuildDiceString(int diceAmount, int diceSize) {
+            string explDice = ExplodingDice ? $"e{diceSize}" : "";
+            return $"{_prefix}{diceAmount}d{diceSize}{explDice}";
+        }
+
+
+        private void OnWindowSizeChanged(object sender, EventArgs e) {
+
+            foreach (Button btn in diceGrid.Children) {
+
+
+                double cellHeight = diceGrid.ActualHeight / diceGrid.RowDefinitions.Count;
+                double cellWidth = diceGrid.ActualWidth / diceGrid.ColumnDefinitions.Count;
+
+                if (cellHeight < 30 || cellWidth < 40) {
+                    btn.Margin = new Thickness(0);
+                    btn.BorderThickness = minBorderThick;
+                }
+                else {
+                    Thickness newMargin = new Thickness(0);
+                    newMargin.Bottom = cellHeight * 0.07f;
+                    newMargin.Top = cellHeight * 0.07f;
+                    newMargin.Left = cellWidth * 0.01f;
+                    newMargin.Right = cellWidth * 0.01f;
+
+                    btn.Margin = newMargin;
+                    btn.BorderThickness = maxBorderThick;
+                }
             }
         }
 
         private void OnButtonClicked(object sender, EventArgs e) {
             if (sender is Button btn) {
                 ChangeBackgroundForDuration(btn, Brushes.LightGreen, 200);
-                Clipboard.SetText($"{CommandTextBox.Text}{btn.Content}");
+                string[] diceInfo = Regex.Split(btn.Content.ToString(), "d");
+                Clipboard.SetText(
+                    BuildDiceString(int.Parse(diceInfo[0]), int.Parse(diceInfo[1]))
+                    );
             }
         }
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e) {
+            base.OnMouseLeftButtonDown(e);
+            _ExplodingDiceButton.Focus();
+        }
 
-        private async Task ChangeBackgroundForDuration(Button btn, Brush newBrush, float durationMs) {            
-            var originalBrush = btn.Background;
+        private async Task ChangeBackgroundForDuration(Button btn, Brush newBrush, float durationMs) {
             btn.Background = newBrush;
             await Task.Delay(TimeSpan.FromMilliseconds(durationMs));
-            btn.Background = originalBrush;
+            btn.Background = Brushes.LightGray;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
--- NicerDicer/Properties/Settings.Designer.cs
+++ NicerDicer/Properties/Settings.Designer.cs
@@ -13,11 +13,11 @@ namespace NicerDicer.Properties {
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
-    internal sealed partial class Settings1 : global::System.Configuration.ApplicationSettingsBase {
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
-        private static Settings1 defaultInstance = ((Settings1)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings1())));
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
-        public static Settings1 Default {
+        public static Settings Default {
             get {
                 return defaultInstance;
             }
@@ -26,12 +26,24 @@ namespace NicerDicer.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("!r ")]
-        public string Command {
+        public string Prefix {
             get {
-                return ((string)(this["Command"]));
+                return ((string)(this["Prefix"]));
             }
             set {
-                this["Command"] = value;
+                this["Prefix"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool ExplodingDice {
+            get {
+                return ((bool)(this["ExplodingDice"]));
+            }
+            set {
+                this["ExplodingDice"] = value;
             }
         }
     }
